987c4fde6481fc2768c8d21730cb405ed6a991b3
sean-halpin <pulpfictionost@gmail.com>
2023-12-16 14:11:30 +0100

    Display Game Timer

--- web_interfaces/ui/src/components/TextComponent.tsx
+++ web_interfaces/ui/src/components/TextComponent.tsx
@@ -1,7 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
 export interface TextProps {
   text: string;
 }
 
 export const TextComponent: React.FC<TextProps> = ({ text }) => {
-  return <div><h3>{text}</h3></div>;
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000); // Update every 1000 milliseconds (1 second)
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []); // Empty dependency array ensures the effect runs only once after initial render
+
+  return (
+    <div className='infoBox'>
+      <p>{text}</p>
+      <p>Timer: {timer} seconds</p>
+    </div>
+  );
 };
