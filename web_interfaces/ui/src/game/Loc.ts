d750093cd9ec25ce577be36babb0cc41280fa2f3
sean-halpin <pulpfictionost@gmail.com>
2023-12-25 12:34:31 +0000

    Loc type Cleanup

--- web_interfaces/ui/src/game/Loc.ts
+++ web_interfaces/ui/src/game/Loc.ts
@@ -1,36 +1,44 @@
 import { None, Option, Some } from "../types/Option";
 
 export class Loc {
-    // #region Constructors (1)
-    constructor(public readonly row: number, public readonly col: number) { }
+  // #region Constructors (1)
 
-    // #endregion Constructors (1)
-    // #region Public Static Methods (1)
-    public static fromNotation(notation: string): Option<Loc> {
-        const column = notation.charCodeAt(0) - 97; // Convert letter to column index (A=0, B=1, ...)
-        const row = parseInt(notation.charAt(1)) - 1; // Convert number to row index (1=0, 2=1, ...)
-        if (isNaN(row) ||
-            isNaN(column) ||
-            row < 0 ||
-            row > 7 ||
-            column < 0 ||
-            column > 7) {
-            return None; // Invalid notation
-        }
+  constructor(readonly row: number, readonly col: number) {}
 
-        return Some(new Loc(row, column));
+  // #endregion Constructors (1)
+
+  // #region Public Static Methods (1)
+
+  public static fromNotation(notation: string): Option<Loc> {
+    const column = notation.charCodeAt(0) - 97; // Convert letter to column index (A=0, B=1, ...)
+    const row = parseInt(notation.charAt(1)) - 1; // Convert number to row index (1=0, 2=1, ...)
+    if (
+      isNaN(row) ||
+      isNaN(column) ||
+      row < 0 ||
+      row > 7 ||
+      column < 0 ||
+      column > 7
+    ) {
+      return None; // Invalid notation
     }
 
-    // #endregion Public Static Methods (1)
-    // #region Public Methods (2)
-    public isEqual(otherLocation: Loc): boolean {
-        return this.row === otherLocation.row && this.col === otherLocation.col;
-    }
+    return Some(new Loc(row, column));
+  }
 
-    public toNotation(): string {
-        const row = this.row + 1;
-        const column = String.fromCharCode(this.col + 97);
-        return `${column}${row}`;
-    }
+  // #endregion Public Static Methods (1)
 
+  // #region Public Methods (2)
+
+  public isEqual(otherLocation: Loc): boolean {
+    return this.row === otherLocation.row && this.col === otherLocation.col;
+  }
+
+  public toNotation(): string {
+    const row = this.row + 1;
+    const column = String.fromCharCode(this.col + 97);
+    return `${column}${row}`;
+  }
+
+  // #endregion Public Methods (2)
 }
